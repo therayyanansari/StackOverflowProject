ba0256b8d2f3bf963a76555a946f29450de9472a
Mohd Rayyan Ansari <rayyanmohdansari@gmail.com>
2021-12-27 10:50:37 +0530

    Initial Commit

---
+++ StackOverflowProject.ServiceLayer/MapperExtensions.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+
+namespace StackOverflowProject.ServiceLayer
+{
+    public static class MapperExtensions
+    {
+        public static void IgnoreUnmappedProperties(TypeMap map, IMappingExpression expr)
+        {
+            foreach(string propname in map.GetUnmappedPropertyNames())
+            {
+                if(map.SourceType.GetProperty(propname) != null)
+                {
+                    expr.ForSourceMember(propname, opt => opt.DoNotValidate());
+                }
+                if(map.DestinationType.GetProperty(propname) != null)
+                {
+                    expr.ForMember(propname, opt => opt.Ignore());
+                }
+
+            }
+        }
+
+        public static void IgnoreUnmapped(this IProfileExpression profile)
+        {
+            profile.ForAllMaps(IgnoreUnmappedProperties);
+        }
+    }
+}
---
+++ StackOverflowProject.ViewModels/EditAnswerViewModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackOverflowProject.ViewModels
+{
+    public class EditAnswerViewModel
+    {
+        [Required]
+        public int AnswerID { get; set; }
+        
+        [Required]
+        public string AnswerText { get; set; }
+        
+        [Required]
+        public DateTime AnswerDateAndTime { get; set; }
+        
+        [Required]
+        public int UserID { get; set; }
+        
+        [Required]
+        public int QuestionID { get; set; }
+        
+        [Required]
+        public int VotesCount { get; set; }
+        
+        [Required]
+        public virtual QuestionViewModel Question { get; set; }
+    }
+}
